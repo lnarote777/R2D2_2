444a3c22a0b6058e44990e6b10edf102ac80443c
lnarote777 <nolara@iesrafaelalberti.es>
2024-01-08 21:16:56 +0100

    r2d2 (hay que mejorarlo)

---
+++ R2D2/src/main/kotlin/Main.kt
@@ -0,0 +1,50 @@
+fun main(args: Array<String>) {
+
+    var r2d2Pos : List<Int>
+    r2d2Pos = moveRobot(10, 5, -2)
+    println("x: ${r2d2Pos[0]}; y: ${r2d2Pos[1]}; dir: ${orientacion(r2d2Pos[2])}")
+
+    r2d2Pos = moveRobot(10, 5, -2)
+    println("x: ${r2d2Pos[0]}; y: ${r2d2Pos[1]}; dir: ${orientacion(r2d2Pos[2])}")
+
+    r2d2Pos = moveRobot(10, 5, -2)
+    println("x: ${r2d2Pos[0]}; y: ${r2d2Pos[1]}; dir: ${orientacion(r2d2Pos[2])}")
+
+    r2d2Pos = moveRobot(10, 5, -2)
+    println("x: ${r2d2Pos[0]}; y: ${r2d2Pos[1]}; dir: ${orientacion(r2d2Pos[2])}")
+}
+
+/**
+ *
+ * @param movs List<Int>
+ */
+fun moveRobot(vararg movs: Int): List<Int>{
+    var posX = 0
+    var posY = 0
+    var dir = 0 //0-> POSITIVEY; 1-> NEGATIVEX; 2-> NEGATIVEY; 3-> POSITIVEX
+
+    for (pasos in movs){
+        when(dir){
+            0 -> posY += pasos
+            1 -> posX -= pasos
+            2 -> posY -= pasos
+            3 -> posX += pasos
+        }
+        if (dir == 3){
+            dir = 0
+        }else{
+            dir ++
+        }
+    }
+    return listOf(posX, posY, dir)
+}
+
+fun orientacion(dir: Int){
+    return when(dir){
+        0 -> "POSITIVEY"
+        1 -> "NEGATIVEX"
+        2-> "NEGATIVEY"
+        3-> "POSITIVEX"
+        else ->
+    }
+}
